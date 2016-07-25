242201095145b864bca1beb34aa08dab281b087b
Nermeen <n.solaiman@gear.it>
2016-07-25 11:32:01 +0200

    Initial Commit

---
+++ android/RadaeePDF-Xamarin/Additions/RadaeePDFPlugin.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Com.Radaee.Pdf;
+using Com.Radaee.Util;
+using Com.Radaee.Reader;
+using Android.App;
+using Android.Text;
+using Android.Content;
+using Android.Webkit;
+using Android.Widget;
+
+namespace RadaeePDF_xamarin.Additions
+{
+    public partial class RadaeePDFPlugin
+    {
+        private Document m_doc;
+
+        public bool activateLicense(Activity mActivity, int licenseType, String company, String email, String key)
+        {
+            Global.MLicenseType = licenseType;
+            Global.MCompany = company;
+            Global.MEmail = email;
+            Global.MKey = key;
+
+            return Global.Init(mActivity, licenseType, company, email, key);
+        }
+
+        public void openFromAssets(String url, String password)
+        {
+            if (!TextUtils.IsEmpty(url))
+            {
+                Context mContext = Android.App.Application.Context;
+                Intent intent = new Intent(mContext, typeof(PDFViewAct));
+                intent.AddFlags(ActivityFlags.NewTask);
+                intent.PutExtra("PDFAsset", url);
+                intent.PutExtra("PDFPswd", password);
+                mContext.StartActivity(intent);
+            }
+        }
+
+        public void show(String url, String password, Activity mActivity)
+        {
+            if(!TextUtils.IsEmpty(url))
+            {
+                Context mContext = Android.App.Application.Context;
+                if(URLUtil.IsFileUrl(url))
+                {
+                    String suffix = "file://";
+                    Intent intent = new Intent(mContext, typeof(PDFViewAct));
+                    intent.AddFlags(ActivityFlags.NewTask);
+                    intent.PutExtra("PDFPath", url.Substring(url.IndexOf(suffix) + suffix.Length));
+                    intent.PutExtra("PDFPswd", password);
+                    mContext.StartActivity(intent);
+                }
+                else if(URLUtil.IsHttpUrl(url) || URLUtil.IsHttpsUrl(url))
+                {
+                    Global.Init(mActivity);
+                    PDFHttpStream m_http_stream = new PDFHttpStream();
+                    m_http_stream.Open(url);
+                    m_doc = new Document();
+                    int ret = m_doc.OpenStreamWithoutLoadingPages(m_http_stream, password);
+                    switch (ret)
+                    {
+                        case -1://need input password
+                            onFail(mContext, "Open Failed: Invalid Password");
+                            break;
+                        case -2://unknown encryption
+                            onFail(mContext, "Open Failed: Unknown Encryption");
+                            break;
+                        case -3://damaged or invalid format
+                            onFail(mContext, "Open Failed: Damaged or Invalid PDF file");
+                            break;
+                        case -10://access denied or invalid file path
+                            onFail(mContext, "Open Failed: Access denied or Invalid path");
+                            break;
+                        case 0://succeeded, and continue
+                            PDFViewAct.MsTranDoc = m_doc;
+                            break;
+                        default://unknown error
+                            onFail(mContext, "Open Failed: Unknown Error");
+                            break;
+                    }
+                    
+                    Intent intent = new Intent(mContext, typeof(PDFViewAct)); 
+                    intent.AddFlags(ActivityFlags.NewTask);
+                    intent.PutExtra("PDFHttp", url);
+                    intent.PutExtra("PDFPswd", password);
+                    mContext.StartActivity(intent);
+                }
+            }
+        }
+
+        private void onFail(Context mContext, String msg)//treat open failed.
+        {
+            m_doc.Close();
+            m_doc = null;
+            Toast.MakeText(mContext, msg, ToastLength.Short).Show();
+        }
+    }
+}
---
+++ android/RadaeePDFDemo/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using RadaeePDF_xamarin.Additions;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace RadaeePDFDemo
+{
+    [Activity(Label = "RadaeePDFDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button activate = FindViewById<Button>(Resource.Id.activate);
+
+            activate.Click += delegate {
+                RadaeePDFPlugin mRadaeePDFPlugin = new RadaeePDFPlugin();
+                bool activated = mRadaeePDFPlugin.activateLicense(this, 2, "radaee", "radaee_com@yahoo.cn", "LNJFDN-C89QFX-9ZOU9E-OQ31K2-FADG6Z-XEBCAO");
+                if (activated)
+                    Toast.MakeText(this, "License activated successfully", ToastLength.Short).Show();
+                else
+                    Toast.MakeText(this, "License activation failure", ToastLength.Short).Show();
+            };
+
+            Button open = FindViewById<Button>(Resource.Id.open);
+
+            open.Click += delegate {
+                RadaeePDFPlugin mRadaeePDFPlugin = new RadaeePDFPlugin();
+                mRadaeePDFPlugin.show("file:///mnt/sdcard/Download/pdf/Facebook Cookbook.pdf", "", this);
+            };
+
+            Button openHttp = FindViewById<Button>(Resource.Id.open_http);
+
+            openHttp.Click += delegate {
+                RadaeePDFPlugin mRadaeePDFPlugin = new RadaeePDFPlugin();
+                mRadaeePDFPlugin.show("http://www.radaeepdf.com/documentation/MRBrochoure.pdf", "", this);
+            };
+
+            Button openAssets = FindViewById<Button>(Resource.Id.open_assets);
+
+            openAssets.Click += delegate {
+                RadaeePDFPlugin mRadaeePDFPlugin = new RadaeePDFPlugin();
+                mRadaeePDFPlugin.openFromAssets("test.PDF", "");
+            };
+        }
+    }
+}
