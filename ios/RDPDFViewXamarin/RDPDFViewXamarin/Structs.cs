f49783c0751ec7a23d16c83dd3365c301163ed29
Federico Vellani <f.vellani@gear.it>
2019-12-06 10:11:37 +0100

    BETA: EXPOSED methods and property from PDFDoc, PDFPage, PDFAnnot PDFLayoutView

--- ios/RDPDFViewXamarin/RDPDFViewXamarin/Structs.cs
+++ ios/RDPDFViewXamarin/RDPDFViewXamarin/Structs.cs
@@ -1,5 +1,56 @@
 ﻿using System;
 
-namespace RDPDFViewXamarin
+namespace RadaeeLib
 {
+	public enum PdfErr : uint
+	{
+		ok = 0,
+		open = 1,
+		password = 2,
+		encrypt = 3,
+		bad_file = 4
+	}
+
+	public enum PdfRenderMode : uint
+	{
+		poor = 0,
+		normal = 1,
+		best = 2
+	}
+
+
+	public struct PDF_SIZE
+	{
+		public float cx;
+
+		public float cy;
+	}
+
+	public struct PDF_POINT
+	{
+		public float x;
+
+		public float y;
+	}
+
+	public struct PDF_RECT
+	{
+		public float left;
+
+		public float top;
+
+		public float right;
+
+		public float bottom;
+	}
+
+	public struct RDVPos
+	{
+		public int pageno;
+
+		public float pdfx;
+
+		public float pdfy;
+	}
+
 }
